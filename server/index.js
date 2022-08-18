3e02eef1ecf3e873851790b0175f958adc24807d
mujahidali88094 <mujahidali88094@gmail.com>
2022-08-18 17:34:15 +0500

    making production ready

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const {Server} = require("socket.io");
 const cors = require("cors");
 const { v4: uuidv4 } = require('uuid');
 
-const port = 3001;
+const port = process.env.PORT | 3001;
 app.use(cors());
 
 const server = http.createServer(app);
